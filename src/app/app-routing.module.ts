278e17baf98172c97285cbe53b0a48831a641a73
shafaz <shafaz@Bashirs-MacBook-Pro.local>
2020-05-21 11:08:50 +0400

    Adding all files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {LoginComponent} from './login/login.component';
+import { RegisterComponent } from  './register/register.component';
+import { FilmsComponent } from  './films/films.component';
+import { FilmViewComponent } from  './films/film-view/film-view.component';
+import { CreateComponent } from  './films/create/create.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'films', pathMatch: 'full' },
+  { path: 'films', component: FilmsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component : RegisterComponent},
+  { path: 'films/create', component : CreateComponent},
+  { path: 'films/:flim_slug_name', component : FilmViewComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { HttpClient,HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+
+import { AuthService } from './services/auth/auth.service';
+import { AuthGuard } from './auth.guard';
+import { AuthInterceptor } from './auth.interceptor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { FilmsComponent } from './films/films.component';
+import { FilmViewComponent } from './films/film-view/film-view.component';
+import { CreateComponent } from './films/create/create.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    FooterComponent,
+    FilmsComponent,
+    FilmViewComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    NgMultiSelectDropDownModule.forRoot(),
+    CommonModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HttpClient,
+    AuthService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
